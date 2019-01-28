ade79c822244eb5f920c225879d0022385cc787f
myra090989 <33969068+myra090989@users.noreply.github.com>
2019-01-27 19:41:51 -0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Myra Sarmiento");
+		System.out.println();
+		System.out.println("loves you ");
+		System.out.println("with all her heart");
+	}
+
+}
